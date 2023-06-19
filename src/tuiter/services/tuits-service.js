8c6e0a63a3afbf6dc24cce38153ace7d47af9728
suisui xia <xiasuisui0402@gmail.com>
2023-06-18 20:21:42 -0700

    a6 mongo su23

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 // const TUITS_API = 'http://localhost:4000/api/tuits';
 // const TUITS_API = 'https://tuiter-node-server-app-49r3.onrender.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
+//console.log(API_BASE);
 const TUITS_API = `${API_BASE}/tuits`;
+//console.log(TUITS_API);
 export const createTuit = async (tuit) => {
  const response = await axios.post(TUITS_API, tuit)
  return response.data;
